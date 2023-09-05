669100a9b3a70b1d18ce4434eeb81657baa5fa36
Thomas Almeida <thomasalmeida.dev@gmail.com>
2023-09-05 10:13:38 -0300

    increase time to suspend in swayidle script

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -2,6 +2,6 @@
 
 swayidle -w timeout 300 'swaylock -C ~/.config/swaylock/config' \
             timeout 600 'hyprctl dispatch dpms off' \
-            timeout 900 'systemctl suspend' \
+            timeout 1200 'systemctl suspend' \
             resume 'hyprctl dispatch dpms on' \
             before-sleep 'sswaylock -C ~/.config/swaylock/config' &
