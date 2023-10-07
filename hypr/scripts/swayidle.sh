51e8d0355418ab9d7565d20ac75763e4ba8e8443
Thomas Almeida <thomasalmeida.dev@gmail.com>
2023-10-07 18:46:46 -0300

    refactor swaylock config

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-isFullScreen() {
-    xdotool search --onlyvisible --class ".*" getwindowgeometry --shell 2>/dev/null | grep "_NET_WM_STATE(ATOM) = _NET_WM_STATE_FULLSCREEN" &> /dev/null
-    return $?
-}
-
-isAppRunning() {
-    local apps=("zoom" "vlc")
-    for app in "${apps[@]}"; do
-        if pgrep -x "$app" > /dev/null; then
-            return 0
-        fi
-    done
-    return 1
-}
-
-shouldLock() {
-    if isFullScreen || isAppRunning; then
-        return 1
-    fi
-    return 0
-}
-
-swayidle -w \
-    timeout 290 'if ! shouldLock; then notify-send -u critical "Locking..." -t 5000; fi' \
-    timeout 295 'if ! shouldLock; then notify-send -u critical "Bye!" -t 5000; fi' \
-    timeout 300 'if ! shouldLock; then swaylock -C ~/.config/swaylock/config; fi' \
-    timeout 600 'if ! shouldLock; then swaymsg "output * dpms off"; fi' \
-    timeout 1200 'if ! shouldLock; then systemctl suspend; fi' \
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock -C ~/.config/swaylock/config' &
+set $lock swaylock -c 550000
+exec swayidle -w \
+  timeout 600 $lock \
+  timeout 570 'swaymsg "output * dpms off"' \
+  resume 'swaymsg "output * dpms on"' \
+  before-sleep $lock
