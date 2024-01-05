086013a869b015d6cc847b13bee10efe1aca8375
Thomas Almeida <thomasalmeida.dev@gmail.com>
2024-01-05 13:49:22 -0300

    Fix: Modify swayidle script to address immediate lock issue

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set $lock swaylock -c 550000
-exec swayidle -w \
-  timeout 600 $lock \
-  timeout 570 'swaymsg "output * dpms off"' \
-  resume 'swaymsg "output * dpms on"' \
-  before-sleep $lock
+start_swayidle() {
+  swayidle -w \
+    timeout 300 'swaylock -C ~/.config/swaylock/config' \
+    timeout 600 'hyprctl dispatch dpms off' \
+    timeout 900 'systemctl suspend' \
+    resume 'hyprctl dispatch dpms on; pkill -f start_swayidle; start_swayidle' \
+    before-sleep 'swaylock -C ~/.config/swaylock/config' &
+}
+
+start_swayidle
