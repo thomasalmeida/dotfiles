3bb9493f4db3acf82cfd996cb4d02c56ecc3fc7e
Thomas Almeida <thomasalmeida.dev@gmail.com>
2024-01-05 14:24:24 -0300

    Refactor: Update VPN script for URL-based authentication

--- waybar/launcher/network/vpn_launcher.sh
+++ waybar/launcher/network/vpn_launcher.sh
@@ -9,16 +9,20 @@ SELECTED_VPN=$(ls $VPN_DIR/*.ovpn | xargs -n 1 basename | wofi -w 3 -n --dmenu -
 
 # Check if a VPN was selected
 if [ -n "$SELECTED_VPN" ]; then
-  # Open Brave browser
-  # brave https://myapps.microsoft.com/ --tile &
-  brave --headless --remote-debugging-port=9222 --disable-gpu https://myapps.microsoft.com/ &
-  BRAVE_PID=$!
-
   # Notify the user that the VPN is connecting
   notify-send "VPN Connection" "Connecting to $SELECTED_VPN..."
 
-  # Start the VPN connection
-  openvpn3 session-start --config "$VPN_DIR/$SELECTED_VPN"
+  # Start the VPN connection and capture the output
+  VPN_OUTPUT=$(openvpn3 session-start --config "$VPN_DIR/$SELECTED_VPN" 2>&1)
+
+  # Check if web authentication is required
+  if echo "$VPN_OUTPUT" | grep -q "Open this URL to complete the connection:"; then
+    # Extract the URL
+    AUTH_URL=$(echo "$VPN_OUTPUT" | grep "https://" | sed 's/.*\(https:\/\/.*\)/\1/')
+
+    # Open the URL in the default browser
+    xdg-open "$AUTH_URL"
+  fi
 
   # Wait for the VPN to connect
   while ! openvpn3 sessions-list | grep -q "Session Path: /net/openvpn/v3/sessions/"; do
@@ -27,12 +31,7 @@ if [ -n "$SELECTED_VPN" ]; then
 
   # Notify the user that the VPN is connected
   notify-send "VPN Connection" "$SELECTED_VPN connected!"
-
-  # Kill the specific Brave instance
-  kill $BRAVE_PID
 else
   # Notify the user that no VPN was selected
   notify-send "VPN Connection" "No VPN selected."
 fi
-
-
