85c1f8b27163498f225d3df58c66b0cea15b1b3a
Thomas Almeida <thomasalmeida.dev@gmail.com>
2024-03-03 12:21:44 -0300

    Refactor Docker service toggle script for improved reliability

--- waybar/launcher/docker/toggle_docker_service.sh
+++ waybar/launcher/docker/toggle_docker_service.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
 # toggle_docker_service.sh
-# Script to toggle the Docker service using Kitty and a temporary file for communication
+# Script to toggle the Docker service
 
 temp_file=$(mktemp /tmp/docker_service_status.XXXXXX)
 
-# Function to execute Docker service command in Kitty, write status to a temp file, and notify
+# Function to execute Docker service command in terminal, write status to a temp file, and notify
 execute_and_notify() {
   local action="$1"
-  local command=""
   local success_flag=""
+  local command_file=$(mktemp)
 
+  # Prepare the command sequence based on the action
   if [[ "$action" == "start" ]]; then
-    command="sudo systemctl start docker && echo 'success' > \"$temp_file\""
+    echo "sudo systemctl start docker" > "$command_file"
     success_flag="Docker service is now running."
   elif [[ "$action" == "stop" ]]; then
-    command="sudo systemctl stop docker.socket && sudo systemctl stop docker && sudo systemctl stop containerd && echo 'success' > \"$temp_file\""
+    echo "sudo systemctl stop docker.socket docker containerd" > "$command_file"
     success_flag="Docker service has been stopped."
   fi
 
-  kitty --title "FloatingKitty" -- bash -c "$command echo 'Press Enter to exit'; read"
+  # Execute each command in the command file
+  while IFS= read -r cmd; do
+    $(head -n 1 ~/dotfiles/.settings/terminal.sh) --title "FloatingTerminal" --command $cmd
+  done < "$command_file"
 
-  # Check the operation result and notify
-  if [[ -f "$temp_file" && $(cat "$temp_file") == 'success' ]]; then
+  # Check the exit status of the last command
+  if [ $? -eq 0 ]; then
+    echo 'success' > "$temp_file"
     notify-send "Docker" "$success_flag"
   else
     notify-send "Docker" "Failed to $action the Docker service."
   fi
 
+  # Clean up
   rm -f "$temp_file"
+  rm -f "$command_file"
 }
 
 # Function to toggle Docker service
